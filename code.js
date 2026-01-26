89734ef2d29162b1aa53fdeb7e05b3be9c2ce058
Sultan <sultanrshah@tutamail.com>
2026-01-26 13:17:22 -0600

    complete assignment
    wow that was pretty easy ngl

--- code.js
+++ code.js
@@ -14,40 +14,49 @@ function getHumanChoice() {
     return input;
 }
 
-let humanScore = 0;
-let computerScore = 0;
+function playGame() {
+    let humanScore = 0;
+    let computerScore = 0;
 
-function playRound(humanChoice, computerChoice) {
-    const fixedHumanChoice = humanChoice.toLowerCase();
+    function playRound(humanChoice, computerChoice) {
+        const fixedHumanChoice = humanChoice.toLowerCase();
     
-    if (computerChoice == fixedHumanChoice) {
-        console.log(`It's a tie! You both picked ` + computerChoice + `!`)
-    } else if (computerChoice == 'rock') {
-        if (humanChoice == 'paper') {
-            console.log('Rock! You win!');
-            humanScore++;
-        } else if (humanChoice == 'scissors') {
-            console.log('Rock! You lose!');
-            computerScore++;
+        if (computerChoice == fixedHumanChoice) {
+                console.log(`It's a tie! You both picked ` + computerChoice + `!`)
+        } else if (computerChoice == 'rock') {
+            if (humanChoice == 'paper') {
+                console.log('Rock! You win!');
+                humanScore++;
+            } else if (humanChoice == 'scissors') {
+                console.log('Rock! You lose!');
+                computerScore++;
+            }
+        } else if (computerChoice == 'paper') {
+            if (humanChoice == 'rock') {
+                console.log('Paper! You lose!');
+                computerScore++;
+            } else if (humanChoice == 'scissors') {
+                console.log('Paper! You win!');
+                humanScore++;
+            }
+        } else if (computerChoice == 'scissors') {
+            if (humanChoice == 'rock') {
+                console.log('Scissors! You win!');
+                humanScore++;
+            } else if (humanChoice == 'paper') {
+                console.log('Scissors! You lose!');
+                computerScore++;
+            }
         }
-    } else if (computerChoice == 'paper') {
-        if (humanChoice == 'rock') {
-            console.log('Paper! You lose!');
-            computerScore++;
-        } else if (humanChoice == 'scissors') {
-            console.log('Paper! You win!');
-            humanScore++;
-        }
-    } else if (computerChoice == 'scissors') {
-        if (humanChoice == 'rock') {
-            console.log('Scissors! You win!');
-            humanScore++;
-        } else if (humanChoice == 'paper') {
-            console.log('Scissors! You lose!');
-            computerScore++;
-        }
+
     }
+
+    for (let i = 0; i < 5; i++) {
+        playRound(getHumanChoice(), getComputerChoice())
+    }
+
+    console.log('Final score is: \nComputer: ' + computerScore + '\nYou: ' + humanScore)
 
 }
 
-playRound(getHumanChoice(), getComputerChoice());
+playGame();
