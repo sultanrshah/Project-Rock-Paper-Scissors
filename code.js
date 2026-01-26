f0490c92547b1e6208a5b8895bdccd75e08a62ff
Sultan <sultanrshah@tutamail.com>
2026-01-26 13:08:57 -0600

    create single loop of game
    take input of human and computer choice, output answer, increment scores accordingly

--- code.js
+++ code.js
@@ -1,5 +1,12 @@
 function getComputerChoice() {
-    return Math.random() * 100;
+    let choice = Math.random() * 100;
+    if (choice < 33) {
+        return `rock`;
+    } else if (choice > 67) {
+        return 'paper';
+    } else {
+        return `scissors`;
+    }
 }
 
 function getHumanChoice() {
@@ -9,3 +16,38 @@ function getHumanChoice() {
 
 let humanScore = 0;
 let computerScore = 0;
+
+function playRound(humanChoice, computerChoice) {
+    const fixedHumanChoice = humanChoice.toLowerCase();
+    
+    if (computerChoice == fixedHumanChoice) {
+        console.log(`It's a tie! You both picked ` + computerChoice + `!`)
+    } else if (computerChoice == 'rock') {
+        if (humanChoice == 'paper') {
+            console.log('Rock! You win!');
+            humanScore++;
+        } else if (humanChoice == 'scissors') {
+            console.log('Rock! You lose!');
+            computerScore++;
+        }
+    } else if (computerChoice == 'paper') {
+        if (humanChoice == 'rock') {
+            console.log('Paper! You lose!');
+            computerScore++;
+        } else if (humanChoice == 'scissors') {
+            console.log('Paper! You win!');
+            humanScore++;
+        }
+    } else if (computerChoice == 'scissors') {
+        if (humanChoice == 'rock') {
+            console.log('Scissors! You win!');
+            humanScore++;
+        } else if (humanChoice == 'paper') {
+            console.log('Scissors! You lose!');
+            computerScore++;
+        }
+    }
+
+}
+
+playRound(getHumanChoice(), getComputerChoice());
